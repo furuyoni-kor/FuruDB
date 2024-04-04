99e02c663201a208a8eb7abb53830bb79a79a511
winters <winters0727@gmail.com>
2024-04-04 19:53:12 +0900

    feat: 필요 상수 정의

---
+++ src/constant/card.ts
@@ -0,0 +1,1 @@
+export const EXCEPTION_CHARACTER = ["kiriko"];
---
+++ src/constant/index.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_LANG = "kor";
+export const DEFAULT_MODE = "O";
+
+export const LOADING_DATA = ["sd_yurina", "sd_saine_a1", "sd_tokoyo_a1"];
