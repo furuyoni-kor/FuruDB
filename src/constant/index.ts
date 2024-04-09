4a42449082dcb62fcdac8a4717127fd2cfb82f2b
winters <winters0727@gmail.com>
2024-04-09 20:01:55 +0900

    feat: I18n 관련 로직 코드 및 데이터 추가

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,4 +1,53 @@
+import type { Metadata } from "next";
+import type { EngCharacterName } from "@/types/character.type";
+
+export const BASE_METADATA: Metadata = {
+  title: "FuruDB - All about Furuyoni, Board Game made by BakaFire Party.",
+  description:
+    "FuruDB - All about Furuyoni, Board Game made by BakaFire Party.",
+  keywords: ["furuyoni", "board game", "database"],
+  creator: "Winters",
+  icons: ["favicon.ico", "/images/twit_icon/21-A1.webp"],
+  openGraph: {
+    title: "FuruDB - All about Furuyoni, Board Game made by BakaFire Party.",
+    description:
+      "FuruDB - All about Furuyoni, Board Game made by BakaFire Party.",
+    images: "/images/twit_icon/21.A1.webp",
+    siteName: "FuruDB",
+  },
+};
+
 export const DEFAULT_LANG = "kor";
 export const DEFAULT_MODE = "O";
 
-export const LOADING_DATA = ["sd_yurina", "sd_saine_a1", "sd_tokoyo_a1"];
+export const LOADING_DATA = ["yurina", "saine_a1", "tokoyo_a1"];
+
+export const MAX_CHARACTER_COUNT = 25;
+
+export const ENG_CHARACTER_NAME: EngCharacterName[] = [
+  "yurina",
+  "saine",
+  "himika",
+  "tokoyo",
+  "oboro",
+  "yukihi",
+  "shinra",
+  "hagane",
+  "chikage",
+  "kururu",
+  "thallya",
+  "raira",
+  "utsuro",
+  "honoka",
+  "korunu",
+  "yatsuha",
+  "hatsumi",
+  "mizuki",
+  "megumi",
+  "kanawe",
+  "kamuwi",
+  "renri",
+  "akina",
+  "shisui",
+  "misora",
+];
--- src/i18n/eng/index.i18n.ts
+++ src/i18n/eng/index.i18n.ts
@@ -1,9 +1,17 @@
+import HeaderJson from "./header.json";
+import FooterJson from "./footer.json";
+import ErrorJson from "./error.json";
 import CardJSON from "./card.json";
 import CharacterJSON from "./character.json";
+import DeckJson from "./deck.json";
 import LoadingJSON from "./loading.json";
 
 export default {
+  header: HeaderJson,
+  footer: FooterJson,
+  error: ErrorJson,
   card: CardJSON,
   character: CharacterJSON,
+  deck: DeckJson,
   loading: LoadingJSON,
 };
