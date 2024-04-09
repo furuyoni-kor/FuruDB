53d4a7ab24f74231186bfd91881fbd399be9d5e7
winters <winters0727@gmail.com>
2024-04-09 20:12:33 +0900

    feat: 작업 페이지 추가

---
+++ src/app/working/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useRef, useEffect } from "react";
+
+import { useI18nContext } from "@/context/i18n.context";
+
+import {
+  WorkingImage,
+  WorkingPageWrapper,
+  WorkingText,
+} from "@/styles/working.style";
+
+import type { NextPage } from "next";
+
+const WorkingPage: NextPage = () => {
+  const classList = ["moveLeft", "workLeft", "moveRight", "workRight"];
+
+  const I18n = useI18nContext();
+
+  const imageRef = useRef<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    let classIndex = 0;
+
+    const intervalId = setInterval(() => {
+      if (imageRef.current) {
+        const image = imageRef.current;
+
+        const prevClass = classList[classIndex];
+        if (classIndex === classList.length - 1) classIndex = -1;
+        const nextClass = classList[++classIndex];
+
+        image.classList.remove(prevClass);
+        image.classList.add(nextClass);
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <WorkingPageWrapper>
+      <WorkingImage
+        ref={imageRef}
+        alt="hagane-chibi"
+        title="working"
+        className="moveLeft"
+        src="/images/chibi/hagane.webp"
+        style={{
+          transform: "scaleX(-1) translateX(-500px)",
+        }}
+      />
+      {(I18n.t("error.work") as string).split("\r\n").map((text) => (
+        <WorkingText>{text}</WorkingText>
+      ))}
+    </WorkingPageWrapper>
+  );
+};
+
+export default WorkingPage;
---
+++ src/styles/working.style.ts
@@ -0,0 +1,90 @@
+import styled, { keyframes } from "styled-components";
+
+const moveLeftAnimation = keyframes`
+  0% {
+    transform: scaleX(-1) translateX(-500px);
+  }
+
+  100% {
+    transform: scaleX(-1) translateX(500px);
+  }
+`;
+
+const moveRightAnimation = keyframes`
+  0% {
+    transform: translateX(-500px);
+  }
+
+  100% {
+    transform: translateX(500px);
+  }
+`;
+
+const workingLeftAnimation = keyframes`
+0% {
+  transform: scaleX(-1) translateX(500px) rotate(0deg);
+}
+
+50% {
+  transform: scaleX(-1) translateX(500px) rotate(30deg);
+}
+
+100% {
+  transform: scaleX(-1) translateX(500px) rotate(0deg);
+}
+`;
+
+const workingRightAnimation = keyframes`
+0% {
+  transform: translateX(500px) rotate(0deg);
+}
+
+50% {
+  transform: translateX(500px) rotate(30deg);
+}
+
+100% {
+  transform: translateX(500px) rotate(0deg);
+}
+`;
+
+export const WorkingPageWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  align-items: center;
+  width: max-content;
+  margin: 64px auto 0 auto;
+  row-gap: 32px;
+`;
+
+export const WorkingImage = styled.img`
+  width: 416px;
+  height: 495px;
+  margin-bottom: 64px;
+
+  &.workLeft {
+    animation: ${workingLeftAnimation} 0.5s infinite alternate;
+  }
+
+  &.workRight {
+    animation: ${workingRightAnimation} 0.5s infinite alternate;
+  }
+
+  &.moveLeft {
+    animation: ${moveLeftAnimation} 1s forwards alternate;
+  }
+
+  &.moveRight {
+    animation: ${moveRightAnimation} 1s forwards alternate;
+  }
+`;
+
+export const WorkingText = styled.div`
+  color: #ffffff;
+  font-family: "SangSangRock";
+  font-size: 4rem;
+  font-weight: 700;
+  text-shadow: 2px 2px #ff64c9;
+  text-align: center;
+`;
